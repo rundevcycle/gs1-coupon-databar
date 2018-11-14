2a35341e43b6332749b427c23fc1dc104892fca0
Andy <andy@rundevcycle.com>
2018-11-13 21:57:42 -0400

    Breaking into modules and adding more tests.

---
+++ src/scripts/gs1Transcoder.js
@@ -0,0 +1,134 @@
+"use strict";
+var GS1Coupon = require('./gs1Coupon');
+
+var GS1Transcoder = function() {
+    this.decode = function(gs1Databar) {
+        console.log("decoder running");
+
+        var couponRegex = /^8110/;
+        if (!couponRegex.test(gs1Databar)) {
+            console.error("Not a North American coupon databar.");
+            return;
+        }
+
+        var gs1Fields = new GS1Coupon();
+
+        var i = 0;
+        gs1Fields.applicationIdentifer = gs1Databar.substring(i, i + 4);
+        i += 4;
+
+        gs1Fields.primaryCompanyPrefixVLI = gs1Databar.substring(i, ++i);
+        gs1Fields.primaryCompanyPrefix = gs1Databar.substring(i, 
+            i + 6 + +gs1Fields.primaryCompanyPrefixVLI);
+        i += 6 + +gs1Fields.primaryCompanyPrefixVLI;
+
+        gs1Fields.offerCode = gs1Databar.substring(i, i + 6);
+        i += 6;
+
+        gs1Fields.saveValueVLI = gs1Databar.substring(i, ++i);
+        gs1Fields.saveValue = gs1Databar.substring(i, 
+            i + +gs1Fields.saveValueVLI);
+        i += +gs1Fields.saveValueVLI;
+
+        gs1Fields.primaryPurchaseRequirementVLI = gs1Databar.substring(i, ++i);
+        gs1Fields.primaryPurchaseRequirement = gs1Databar.substring(i, 
+            i + +gs1Fields.primaryPurchaseRequirementVLI);
+        i += +gs1Fields.primaryPurchaseRequirementVLI;
+
+        gs1Fields.primaryPurchaseRequirementCode = gs1Databar.substring(i, ++i);
+
+        gs1Fields.primaryPurchaseFamilyCode = gs1Databar.substring(i, i + 3);
+        i += 3;
+
+        let optionalFieldIndctr = gs1Databar.substring(i, ++i);
+        
+        while (+optionalFieldIndctr) {
+            switch (+optionalFieldIndctr) {
+                case 1:
+                    // Second qualifying purchase
+                    console.log("Second qualifying item");
+                    gs1Fields.secondaryAdditionalPurchaseRulesCode = gs1Databar.substring(i, ++i);
+                    gs1Fields.secondaryPurchaseRequirementVLI = gs1Databar.substring(i, ++i);
+                    gs1Fields.secondaryPurchaseRequirement = gs1Databar.substring(i, 
+                        i + +gs1Fields.secondaryPurchaseRequirementVLI);
+                    i += +gs1Fields.secondaryPurchaseRequirementVLI;
+        
+                    gs1Fields.secondaryPurchaseRequirementCode = gs1Databar.substring(i, ++i);        
+                    gs1Fields.secondaryPurchaseFamilyCode = gs1Databar.substring(i, i + 3); 
+                    i += 3;
+        
+                    gs1Fields.secondaryPurchaseCompanyPrefixVLI = gs1Databar.substring(i, ++i);        
+                    gs1Fields.secondaryPurchaseCompanyPrefix = gs1Databar.substring(i,
+                        i + 6 + +gs1Fields.secondaryPurchaseCompanyPrefixVLI);
+                    i += 6 + +gs1Fields.secondaryPurchaseCompanyPrefixVLI;
+                    break;
+
+                case 2:
+                    // Third qualifying purchase
+                    console.log("Third qualifying item");
+                    gs1Fields.tertiaryPurchaseRequirementVLI = gs1Databar.substring(i, ++i);        
+                    gs1Fields.tertiaryPurchaseRequirement = gs1Databar.substring(i, 
+                        i + +gs1Fields.tertiaryPurchaseRequirementVLI);
+                    i += +gs1Fields.tertiaryPurchaseRequirementVLI;
+        
+                    gs1Fields.tertiaryPurchaseRequirementCode = gs1Databar.substring(i, ++i);        
+                    gs1Fields.tertiaryPurchaseFamilyCode = gs1Databar.substring(i, i + 3); 
+                    i += 3;
+        
+                    gs1Fields.tertiaryPurchaseCompanyPrefixVLI = gs1Databar.substring(i, ++i);        
+                    gs1Fields.tertiaryPurchaseCompanyPrefix = gs1Databar.substring(i,
+                        i + 6 + +gs1Fields.tertiaryPurchaseCompanyPrefixVLI);
+                    i += 6 + +gs1Fields.tertiaryPurchaseCompanyPrefixVLI;
+                    break;
+
+                case 3:
+                    // Expiry date
+                    console.log("Expiration date");
+                    gs1Fields.expirationDate = gs1Databar.substring(i, i + 6);
+                    i += 6;
+                    break;
+
+                case 4:
+                    // Start date
+                    console.log("Start date");
+                    gs1Fields.startDate = gs1Databar.substring(i, i + 6);
+                    i += 6;
+                    break;
+
+                case 5:
+                    // Serial number
+                    console.log("Serial number");
+                    gs1Fields.serialNumberVLI = gs1Databar.substring(i, ++i);
+                    gs1Fields.serialNumber = gs1Databar.substring(i, 
+                        i + 6 + +gs1Fields.serialNumberVLI);
+                    i += 6 + +gs1Fields.serialNumberVLI;
+                    break;
+
+                case 6:
+                    // Retailer identification
+                    console.log("Retailer identification");
+                    gs1Fields.retailerCompanyPrefixVLI = gs1Databar.substring(i, ++i);
+                    gs1Fields.retailerCompanyPrefixOrGLN = gs1Databar.substring(i,
+                        i + 7 + +gs1Fields.retailerCompanyPrefixVLI);
+                    i += 7 + +gs1Fields.retailerCompanyPrefixVLI;
+                    break;
+
+                case 9:
+                    // Miscellaneous
+                    console.log("Miscellaneous");
+                    gs1Fields.saveValueCode = gs1Databar.substring(i, ++i);
+                    gs1Fields.saveValueAppliesToWhichItem = gs1Databar.substring(i, ++i);
+                    gs1Fields.storeCouponFlag = gs1Databar.substring(i, ++i);
+                    gs1Fields.doNotMultiplyFlag = gs1Databar.substring(i, ++i);
+                    break;
+            }
+
+            optionalFieldIndctr = gs1Databar.substring(i, ++i);
+        }
+
+        console.log(gs1Fields);
+        return gs1Fields;
+    }
+};
+
+module.exports = GS1Transcoder;
--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 var assert = require('assert');
+var GS1Coupon = require('../src/scripts/gs1Coupon');
+var GS1Transcoder = require('../src/scripts/gs1Transcoder');
 
 describe('Array', function() {
     describe('#indexOf', function() {
@@ -12,13 +14,30 @@ describe('Array', function() {
 describe('GS1 Decoder', function() {
     describe('#Minimal Valid GS1', function() {
         it('should pass for a minimal GS1 code', function() {
-            let gs1Transcoder = new GS1Transcoder();
+            let transcoder = new GS1Transcoder();
             const barcode = "8110012345698765432505223344666";
-            let gs1Fields = gs1Transcoder.decode(barcode);
+            let gs1Fields = transcoder.decode(barcode);
             assert.equal(gs1Fields.primaryCompanyPrefix, "123456");
             assert.equal(gs1Fields.offerCode, "987654");
             assert.equal(gs1Fields.primaryPurchaseFamilyCode, "666");
         });
+    });
 
+    describe('#With Expiry Date', function() {
+        it('should pass', function() {
+            let transcoder = new GS1Transcoder();
+            const barcode = "81100123456987654325052233446663181231";
+            let gs1Fields = transcoder.decode(barcode);
+            assert.equal(gs1Fields.expirationDate, "181231");
+        });
+    });
+
+    describe('#With Start Date', function() {
+        it('should pass', function() {
+            let transcoder = new GS1Transcoder();
+            const barcode = "81100123456987654325052233446664180701";
+            let gs1Fields = transcoder.decode(barcode);
+            assert.equal(gs1Fields.startDate, "180701");
+        });
     });
 });
